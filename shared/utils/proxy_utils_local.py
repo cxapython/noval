8e7a942e65288766920ab992f5a4e66ae00b40b7
cxapython <cxapython@gmail.com>
2025-10-05 08:22:44 +0800

    添加配置文件
    - 添加 config.py（数据库配置）
    - 添加 proxy_utils_local.py（本地代理配置）
    - 更新 .gitignore（保留proxy_utils.py排除）

---
+++ shared/utils/proxy_utils_local.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class ProxyUtils:
+    def get_proxy(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    proxy_util = ProxyUtils()
+    print(proxy_util.get_proxy())
